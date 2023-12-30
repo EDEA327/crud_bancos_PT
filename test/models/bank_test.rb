e40541141d4abd5d6cf81a2f79c188e2284c703a
Erick Escobar <edeajob27@gmail.com>
2023-12-30 16:04:55 -0500

    Probando el modelo Bank

--- test/models/bank_test.rb
+++ test/models/bank_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "El banco debe tener nombre" do
+    bank = Bank.new(name: '')
+    assert_not bank.valid?, 'Nombre en blanco o no válido'
+  end
+
+  test "El nombre no debe exceder los 50 caracteres" do
+    bank = Bank.new(name: 'a' * 51)
+    assert_not bank.valid?, 'nombre length not validated'
+  end
 end
