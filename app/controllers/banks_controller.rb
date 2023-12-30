da1e7baa9d025c06c3326c75b57f883e53797479
Erick Escobar <edeajob27@gmail.com>
2023-12-30 17:20:48 -0500

    Cree un test para ver si el controlador permite crear un banco con un nombre de más de 50 caracteres

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -7,4 +7,23 @@ class BanksController < ApplicationController
     @bank = Bank.find(params[:id])
   end
 
+  def new
+    @bank = Bank.new
+  end
+
+  def create
+    @bank = Bank.new(bank_params)
+
+    if @bank.save
+      redirect_to @bank, notice: 'El banco fue creado exitosamente.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bank_params
+    params.require(:bank).permit(:name, :other_attributes)
+  end
 end
--- test/controllers/banks_controller_test.rb
+++ test/controllers/banks_controller_test.rb
@@ -1,7 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class BanksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "No deberia crear un banco con un nombre de más de 50 caracteres" do
+    assert_no_difference('Bank.count') do
+      post banks_url, params: { bank: invalid_long_name_attributes }
+    end
+
+    assert_template "new"
+    assert_instance_of Bank, assigns(:bank)
+    assert assigns(:bank).new_record?
+    assert_equal ["is too long (maximum is 50 characters)"], assigns(:bank).errors[:name]
+  end
+
+  private
+
+  def invalid_long_name_attributes
+    { name: "A" * 51}
+  end
 end
