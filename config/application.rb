2d85e5836fa1d0720ca99e012e7e9b502699bf82
Erick Escobar <edeajob27@gmail.com>
2023-12-31 17:17:12 -0500

    Configuracion de i18N

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module EntregablePtHt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # availabe languages
+    config.i18n.available_locales = [:en, :es]
+
+    # language by default
+    config.i18n.default_locale = :es
+
   end
 end
