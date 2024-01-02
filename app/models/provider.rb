bab5489514b8e3e12df3ac506a3cd42a2ee907e5
Erick Escobar <edeajob27@gmail.com>
2024-01-02 18:06:24 -0500

    Importe I18N ya que los test no pasaban por ese error

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ApplicationRecord
   has_one :bank_account
   validates :name, :nit, :contact_name, presence: true
-  validates :nit, format: { with: /\A\d{9}-\d?\z/, message: t('providers.errors.nit') }
+  validates :nit, format: { with: /\A\d{9}-\d?\z/, message: I18n.t('providers.errors.nit') }
   validates :contact_phone, length: { maximum: 10 }, allow_blank: true
 end
