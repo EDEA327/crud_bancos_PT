3b198643b6ba80739f89c6c4e496c2e9324d320f
Erick Escobar <edeajob27@gmail.com>
2024-01-02 17:56:33 -0500

    Creación de los test del controlador de proveedores

--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -1,28 +1,71 @@
 require "test_helper"
 
 class ProvidersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  setup do
+    @provider = providers(:one)
+  end
+
+  test "debería obtener nueva" do
     get providers_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test "debería obtener crear" do
     get providers_create_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test "debería obtener editar" do
     get providers_edit_url
     assert_response :success
   end
 
-  test "should get update" do
+  test "debería obtener actualizar" do
     get providers_update_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test "debería obtener destruir" do
     get providers_destroy_url
     assert_response :success
+  end
+
+  test "debería obtener índice" do
+    get providers_url
+    assert_response :success
+  end
+
+  test "debería crear proveedor" do
+    assert_difference('Provider.count') do
+      post providers_url, params: { provider: { name: 'Nuevo Proveedor', nit: '123456789-0', contact_name: 'John Doe' } }
+    end
+
+    assert_redirected_to provider_url(Provider.last)
+    assert_equal 'El proveedor fue creado exitosamente.', flash[:notice]
+  end
+
+  test "debería mostrar proveedor" do
+    get provider_url(@provider)
+    assert_response :success
+  end
+
+  test "debería obtener editar" do
+    get edit_provider_url(@provider)
+    assert_response :success
+  end
+
+  test "debería actualizar proveedor" do
+    patch provider_url(@provider), params: { provider: { name: 'Proveedor Actualizado' } }
+    assert_redirected_to providers_url
+    assert_equal 'El proveedor se actualizó exitosamente.', flash[:notice]
+  end
+
+  test "debería destruir proveedor" do
+    assert_difference('Provider.count', -1) do
+      delete provider_url(@provider)
+    end
+
+    assert_redirected_to providers_url
+    assert_equal 'El proveedor se eliminó exitosamente.', flash[:notice]
   end
 end
