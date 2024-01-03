77f9924877c24809ba55ee0b4f70f71f571202d2
Erick Escobar <edeajob27@gmail.com>
2024-01-03 12:45:30 -0500

    Cambie algunas traducciones para que sean más faciles de acceder

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,8 +1,12 @@
 class Provider < ApplicationRecord
-  validates :name, :nit, :contact_name, presence: true
-  validates :nit, format: { with: /\A\d{9}-\d?\z/, message: I18n.t('providers.errors.nit') }
+  validates :name, :contact_name, :bank_name, presence: true
+  validates :nit, format: { with: /\A\d{9}-\d{1}?\z/ }, uniqueness: true
   validates :contact_phone, length: { maximum: 10 }, allow_blank: true
-  # Relacion con la cuenta bancaria
+
   has_one :bank_account, dependent: :destroy
   accepts_nested_attributes_for :bank_account
+
+  def self.available_banks
+    Bank.pluck(:name)
+  end
 end
