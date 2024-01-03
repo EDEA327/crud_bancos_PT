0376ede4e16c3ce5cd111d4fbfd0c1eeab4011ea
Erick Escobar <edeajob27@gmail.com>
2024-01-03 14:07:06 -0500

    Error en el nombre del modelo

--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -1,51 +1,53 @@
+# test/controllers/providers_controller_test.rb
+
 require 'test_helper'
 
-class ProveedoresControllerTest < ActionDispatch::IntegrationTest
+class ProvidersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @proveedor = providers(:one)
+    @provider = providers(:one)
   end
 
   test 'debería obtener el índice' do
-    get proveedores_url
+    get providers_url
     assert_response :success
   end
 
   test 'debería obtener nuevo' do
-    get new_proveedor_url
+    get new_provider_url
     assert_response :success
   end
 
   test 'debería crear proveedor' do
     assert_difference('Provider.count') do
-      post proveedores_url, params: { provider: { name: 'Nuevo Proveedor', nit: '123456789-0', contact_name: 'Nuevo Contacto', contact_phone: '1234567890', bank_id: banks(:one).id } }
+      post providers_url, params: { provider: { name: 'Nuevo Proveedor', nit: '123456789-0', contact_name: 'Nuevo Contacto', contact_phone: '1234567890', bank_id: banks(:one).id } }
     end
 
-    assert_redirected_to proveedor_url(Provider.last)
+    assert_redirected_to provider_url(Provider.last)
     assert_equal I18n.t('providers.create.success'), flash[:notice]
   end
 
   test 'debería mostrar proveedor' do
-    get proveedor_url(@proveedor)
+    get provider_url(@provider)
     assert_response :success
   end
 
   test 'debería obtener editar' do
-    get edit_proveedor_url(@proveedor)
+    get edit_provider_url(@provider)
     assert_response :success
   end
 
   test 'debería actualizar proveedor' do
-    patch proveedor_url(@proveedor), params: { provider: { name: 'Proveedor Actualizado' } }
-    assert_redirected_to proveedor_url(@proveedor)
+    patch provider_url(@provider), params: { provider: { name: 'Proveedor Actualizado' } }
+    assert_redirected_to provider_url(@provider)
     assert_equal I18n.t('providers.update.success'), flash[:notice]
   end
 
   test 'debería destruir proveedor' do
     assert_difference('Provider.count', -1) do
-      delete proveedor_url(@proveedor)
+      delete provider_url(@provider)
     end
 
-    assert_redirected_to proveedores_url
+    assert_redirected_to providers_url
     assert_equal I18n.t('providers.destroy.success'), flash[:notice]
   end
 end
