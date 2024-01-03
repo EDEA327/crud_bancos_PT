fefa4d3da4a80b8923d3e95e994ff849757432f1
Erick Escobar <edeajob27@gmail.com>
2024-01-03 14:04:56 -0500

    Cree el controlador asi como su test

---
+++ app/controllers/providers_controller.rb
@@ -0,0 +1,50 @@
+class ProvidersController < ApplicationController
+  before_action :set_provider, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @providers = Provider.all
+  end
+
+  def show
+  end
+
+  def new
+    @provider = Provider.new
+  end
+
+  def create
+    @provider = Provider.new(provider_params)
+
+    if @provider.save
+      redirect_to @provider, notice: I18n.t('providers.create.success')
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @provider.update(provider_params)
+      redirect_to @provider, notice: I18n.t('providers.update.success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @provider.destroy
+    redirect_to providers_url, notice: I18n.t('providers.destroy.success')
+  end
+
+  private
+
+  def set_provider
+    @provider = Provider.find(params[:id])
+  end
+
+  def provider_params
+    params.require(:provider).permit(:name, :nit, :contact_name, :contact_phone, :bank_id)
+  end
+end
