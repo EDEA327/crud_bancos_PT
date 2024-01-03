6c8bb7e042151ab8309bc637ec1041b925498aba
Erick Escobar <edeajob27@gmail.com>
2024-01-03 15:39:50 -0500

    hice que el numeor de cuenta sea accesible para el modelo proveedor

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -5,4 +5,8 @@ class Provider < ApplicationRecord
   validates :nit, format: { with: /\A\d{9}-\d?\z/, message: 'must be in the format 123456789-0' }, allow_blank: true, uniqueness: true
   validates :contact_name, presence: true, length: { maximum: 255 }
   validates :contact_phone, length: { maximum: 10 }, allow_blank: true
+
+  def bank_account_number
+    bank.account_number
+  end
 end
