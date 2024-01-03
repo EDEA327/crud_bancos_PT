237302939a4d97f8bfd6feef210b39db85dc0b92
Erick Escobar <edeajob27@gmail.com>
2024-01-03 10:35:00 -0500

    Actualice el modelo provider para relacionarlo con el modelo bank_account

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount < ApplicationRecord
-  belongs_to :provider
   validates :bank_name, presence: true
   validates :account_number, length: { maximum: 15 }, allow_blank: true
+
+  belongs_to :provider
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,8 @@
 class Provider < ApplicationRecord
-  belongs_to :bank
-  has_one :bank_account
   validates :name, :nit, :contact_name, presence: true
   validates :nit, format: { with: /\A\d{9}-\d?\z/, message: I18n.t('providers.errors.nit') }
   validates :contact_phone, length: { maximum: 10 }, allow_blank: true
+  # Relacion con la cuenta bancaria
+  has_one :bank_account, dependent: :destroy
+  accepts_nested_attributes_for :bank_account
 end
