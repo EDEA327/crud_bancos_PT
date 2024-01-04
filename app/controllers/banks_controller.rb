130ff7da7d38aceb63710e1776489df74a2992ce
Erick Escobar <edeajob27@gmail.com>
2024-01-03 20:25:31 -0500

    Aplicando restriccion para las acciones de Banks comtroller

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,5 @@
 class BanksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   def index
     @banks = Bank.all
   end
