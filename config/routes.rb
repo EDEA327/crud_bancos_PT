6e7039aa588121c49939a09883975295bc319efe
Erick Escobar <edeajob27@gmail.com>
2024-01-03 20:26:23 -0500

    Intento de corregir un erro en las rutas cuando se hace sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
   get '/dashboard', to: 'dashboard#show', as: 'dashboard'
+  devise_for :users
   resources :banks
   resources :providers
 end
