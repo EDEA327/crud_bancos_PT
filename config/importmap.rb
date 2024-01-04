d1f16365c29127a5e042bcd9cbab5f65cd8d381b
Erick Escobar <edeajob27@gmail.com>
2024-01-04 11:23:22 -0500

    Importamos bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,9 @@
 # Pin npm packages by running ./bin/importmap
-
+# config/importmap.rb
+# Bootstrap CSS
+import "bootstrap";
+# Bootstrap JavaScript
+import "popper";
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
