2068376b50b89828be7a8b2874115677bff5182b
Erick Escobar <edeajob27@gmail.com>
2024-01-03 19:52:30 -0500

    Haciendo sign_uop antes de que se ejecuten los test

--- test/controllers/banks_controller_test.rb
+++ test/controllers/banks_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class BanksControllerTest < ActionDispatch::IntegrationTest
-  test "No deberia crear un banco con un nombre de más de 50 caracteres" do
+  setup do
+    @user = users(:one)
+    sign_in(@user)
+  end
+
+  test "No debería crear un banco con un nombre de más de 50 caracteres" do
     assert_no_difference('Bank.count') do
       post banks_url, params: { bank: invalid_long_name_attributes }
     end
@@ -12,7 +17,7 @@ class BanksControllerTest < ActionDispatch::IntegrationTest
     assert_equal [I18n.t('activerecord.errors.models.bank.attributes.name.too_long')], assigns(:bank).errors[:name]
   end
 
-  test "Deberia mostrar la lista de bancos" do
+  test "Debería mostrar la lista de bancos" do
     get banks_url
     assert_response :success
   end
@@ -28,28 +33,41 @@ class BanksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Debe crear un banco" do
-    post banks_url, params: { bank: { name: "New Bank" } }
+    assert_difference('Bank.count') do
+      post banks_url, params: { bank: { name: "New Bank" } }
+    end
+
     assert_redirected_to banks_url
     assert_equal flash[:notice], I18n.t('banks.created')
   end
 
   test "Debe actualizar el banco" do
-    patch bank_url(banks(:one)), params: { bank: { name: "Updated Bank" } }
-    assert_redirected_to banks_url
+    put bank_url(banks(:one)), params: { bank: { name: "Updated Bank" } }
+
+    assert_response :redirect # Verifica que la respuesta sea una redirección
+
+    follow_redirect! # Sigue la redirección
+
+    assert_equal I18n.t('banks.updated'), flash[:notice] # Verifica el mensaje de notificación
+
+    # Verifica que la actualización haya ocurrido correctamente
+    assert_equal "Updated Bank", banks(:one).reload.name
   end
+
+
 
   test "Debe eliminar el banco" do
     assert_difference('Bank.count', -1) do
       delete bank_path(banks(:one))
     end
 
-    assert_redirected_to banks_path
+    assert_redirected_to banks_url
     assert_equal flash[:notice], I18n.t('banks.destroyed')
   end
 
   private
 
   def invalid_long_name_attributes
-    { name: "A" * 51}
+    { name: "A" * 51 }
   end
 end
